22a7e479fd5e1625b0cd5c764c3f7dbdd2e950f6
Patrick Steadman <patrick.steadman@sothebys.com>
2022-01-12 18:17:56 -0500

    Add instanceof tests
    Signed-off-by: Patrick Steadman <patrick.steadman@sothebys.com>

--- examples/jest/ts/src/index.ts
+++ examples/jest/ts/src/index.ts
@@ -1,3 +1,10 @@
 import extra from './extra';
 
 export default extra;
+
+export class CustomError extends Error {
+  constructor(m: string) {
+    super(m);
+    Object.setPrototypeOf(this, CustomError.prototype);
+  }
+}
--- examples/jest/ts/test/index.test.ts
+++ examples/jest/ts/test/index.test.ts
@@ -1,5 +1,19 @@
-import indexfile from 'lib';
+import indexfile, { CustomError } from 'lib';
+
+export class CustomErrorNoImport extends Error {
+  constructor(m: string) {
+    super(m);
+  }
+}
 
 test('it should work', () => {
   expect(indexfile).toBe('test');
+});
+
+test('instanceof should work', () => {
+  expect((new CustomError('foo')) instanceof CustomError).toBeTruthy();
+});
+
+test('instanceof inside file should work', () => {
+  expect((new CustomErrorNoImport('foo')) instanceof CustomErrorNoImport).toBeTruthy();
 });
